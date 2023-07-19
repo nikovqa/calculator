62a33c6ce97aa91b17f1e0f6947194302aacfef6
Nikolay <dojazz@icloud.com>
2023-07-19 11:05:55 +0300

    добавлены коментарии

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
     public class Main {
         public static void main(String[] args) {
-            System.out.println("Введите два целых числа меньше или равные 10, между ними должен быть оператор (+, -, /, *), в качестве разделителя используйте пробел\nПРИМЕР: 10 * 10");
+            System.out.println("Введите два целых числа от 1 до 10, между ними должен быть оператор (+, -, /, *), в качестве разделителя используйте пробел\nПРИМЕР: 10 * 10");
             Scanner scanner = new Scanner(System.in);
             String input = scanner.nextLine();
             String result = calc(input);
@@ -14,15 +14,15 @@ import java.util.Scanner;
         public static String calc(String input) {
             String[] data = input.split(" ");
             if (data.length !=3) {
-                throw new IllegalArgumentException("Invalid input: На ввод принимается два целых числа меньше или равные 10 и один оператор (+, -, /, *) разделенные пробелом\nПРИМЕР: 10 + 10");
+                throw new IllegalArgumentException("Invalid input: На ввод принимается два целых числа от 1 до 10 и один оператор (+, -, /, *) разделенные пробелом\nПРИМЕР: 10 + 10");
             }
             int a = Integer.parseInt(data[0]);
             if (a < 1 || a > 10) {
-                throw new IllegalArgumentException("Invalid input: Числа должны быть меньше или равны 10");
+                throw new IllegalArgumentException("Invalid input: Числа должны быть от 1 до 10");
             }
             int b = Integer.parseInt(data[2]);
             if (b < 1 || b > 10) {
-                throw new IllegalArgumentException("Invalid input: Числа должны быть меньше или равны 10");
+                throw new IllegalArgumentException("Invalid input: Числа должны быть от 1 до 10");
             }
             int result;
             switch (data[1]) {
@@ -39,7 +39,7 @@ import java.util.Scanner;
                     result = a / b;
                     break;
                 default:
-                    throw new IllegalArgumentException("Invalid input: На ввод принимается два целых числа меньше или равные 10 и один оператор (+, -, /, *) разделенные пробелом\nПРИМЕР: 10 + 10");
+                    throw new IllegalArgumentException("Invalid input: На ввод принимается два целых числа от 1 до 10 и один оператор (+, -, /, *) разделенные пробелом\nПРИМЕР: 10 + 10");
             }
             return String.valueOf(result);
         }
